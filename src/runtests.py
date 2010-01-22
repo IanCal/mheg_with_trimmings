eb8b31804028e7644ab7b3bc486ccb1e324576a5
Ian Calvert <ianjcalvert@googlemail.com>
2010-01-22 15:21:03 +0000

    Have added basic parsing and a test case for IF blocks. Currently doesn't actually parse the IF blocks, so can't tell that if statements need a condition

--- src/runtests.py
+++ src/runtests.py
@@ -1,25 +1,56 @@
 import unittest
-import parseFunctions
+from parseFunctions import *
 
-class IfTests(unittest.TestCase):
-    def testIfExists(self):
-        self.assertFalse(parseFunctions.lineContainsIf(""))
-        self.assertFalse(parseFunctions.lineContainsIf("  "))
-        self.assertFalse(parseFunctions.lineContainsIf("                              "))
-        self.assertFalse(parseFunctions.lineContainsIf("{:ifs"))
-        #Passing cases
-        self.assertTrue(parseFunctions.lineContainsIf("{:if"))
-        self.assertTrue(parseFunctions.lineContainsIf("{:IF"))
-        self.assertTrue(parseFunctions.lineContainsIf("{:If"))
-        self.assertTrue(parseFunctions.lineContainsIf("{:iF"))
-        self.assertTrue(parseFunctions.lineContainsIf("    {:if"))
-        self.assertTrue(parseFunctions.lineContainsIf("    {:if  #comment"))
+validIfBlocks = [
+        """
+        {:if
+            :condition ( X < 3 )
+            {:iftrue
+                Y = 3;
+                X = 2;
+            }
+            {:iffalse
+                Y = 10;
+                X = 3;
+            }
+        ]
+        """,
+        """
+        {:If
+            :Condition ( Y  >= 2 )
+            {:ifTrue
+                X = 3;
+            }
+        }
+        """
+        ]
+invalidIfBlocks = [
+        """
+        {if
+        :condition
+        }""",
+        """
+        {:If
+            :Condition
+            {:ifTrue
+                X = 3;
+            }
+        }
+        """
+        ]
 
-class VariableTests(unittest.TestCase):
-    def testSingle(self):
-        self.failUnless(False)
-    def testRestrictedKeyword(self):
-        self.failUnless(False)
+class TestBlockExtraction(unittest.TestCase):
+    def testExtractValidIf(self):
+        for validBlock in validIfBlocks:
+            block = parse(validBlock)
+            self.assertTrue(isinstance(block, BasicBlock))
+            self.assertTrue(isinstance(block.code[0], IfBlock), "Thinks this isn't an IF block: " + validBlock)
+    def testExtractInvalidIf(self):
+        for invalidBlock in invalidIfBlocks:
+            block = parse(invalidBlock)
+            self.assertTrue(isinstance(block, BasicBlock))
+            self.assertFalse(isinstance(block.code[0], IfBlock), "Thinks this is a valid IF block: " + invalidBlock)
+
 
 def main():
     unittest.main()
